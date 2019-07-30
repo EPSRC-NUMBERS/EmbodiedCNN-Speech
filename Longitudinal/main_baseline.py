3390898d99ec905b4889fbae65037345861ef5ff
EPSRC NUMBERS Project <41962938+EPSRC-NUMBERS@users.noreply.github.com>
2019-07-30 16:41:12 +0200

    Update main_baseline.py

--- Longitudinal/main_baseline.py
+++ Longitudinal/main_baseline.py
@@ -27,22 +27,6 @@ EPOCHS = 50
 LABELS = 'one two three four five six seven eight nine'.split()
 NUM_CLASSES = len(LABELS)
 
-#==============================================================================
-# Prepare data      
-# #==============================================================================
-# dsGen = DatasetGenerator(label_set=LABELS) 
-# # # Load DataFrame with paths/labels for training and validation data 
-# # # and paths for testing data 
-# df = dsGen.load_data(DIR)
-
-# dsGen.apply_train_test_split(test_size=0.0, random_state=2018)
-# dsGen.apply_train_val_split(val_size=0.2, random_state=2018)
-
-# dsGen.build_dataset(mode='train')
-# dsGen.build_dataset(mode='val')
-
-# sys.exit()
-
 x_train = np.load('train_batches.npy')
 y_train = np.load('train_targets.npy')
 x_test = np.load('val_batches.npy')
